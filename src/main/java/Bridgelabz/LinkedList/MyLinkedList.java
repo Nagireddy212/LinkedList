3962089e26070c9b8426cd540d2091639e3bb58a
Ngireddy <somulanagireddy@gmail.com>
2021-04-02 13:03:58 +0530

    [Nagireddy]: added final Sequence

--- src/main/java/Bridgelabz/LinkedList/MyLinkedList.java
+++ src/main/java/Bridgelabz/LinkedList/MyLinkedList.java
@@ -1,39 +1,58 @@
 package Bridgelabz.LinkedList;
 
 public class MyLinkedList<T> {
-    private INode head;
-    private INode tail;
+    private INode<T> head;
+    private INode<T> tail;
 
     public MyLinkedList() {
         this.head = null;
         this.tail = null;
     }
 
-    public INode getHead() {
+    public INode<T> getHead() {
         return head;
     }
 
-    public void setHead(INode head) {
+    public void setHead(INode<T> head) {
         this.head = head;
     }
 
-    public INode getTail() {
+    public INode<T> getTail() {
         return tail;
     }
 
-    public void setTail(INode tail) {
+    public void setTail(INode<T> tail) {
         this.tail = tail;
     }
 
-    public void append(INode nodeToAppend) {
+    public void append(INode<T> nodeToAppend) {
         if (this.head == null){
             this.head = nodeToAppend;
         }
-        if(this.tail == null){
-            this.tail = nodeToAppend;
-        }else{
+        if (this.tail != null) {
             this.tail.setNext(nodeToAppend);
-            this.tail = nodeToAppend;
         }
+        this.tail = nodeToAppend;
+    }
+
+    public void insert(INode<T> nodeToInsertInBetweenList , T previousNodeData) {
+        INode currentNode = this.head;
+        while(!currentNode.getData().equals(previousNodeData)){
+            currentNode = currentNode.getNext();
+        }
+        INode nodeAfterInsertedNode = currentNode.getNext();
+        currentNode.setNext(nodeToInsertInBetweenList);
+        nodeToInsertInBetweenList.setNext(nodeAfterInsertedNode);
+    }
+
+    public void printList() {
+        INode currentNode = this.head;
+        System.out.print("Head->  ");
+        while(currentNode.getNext() != null){
+            System.out.print(currentNode.getData()+"->");
+            currentNode = currentNode.getNext();
+        }
+        System.out.print(currentNode.getData());
+        System.out.println("  <-Tail");
     }
 }
