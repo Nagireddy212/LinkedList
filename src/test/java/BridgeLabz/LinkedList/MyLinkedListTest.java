ba136abf06e214e8279464e4a74eb611b40cd6e8
Ngireddy <somulanagireddy@gmail.com>
2021-04-02 13:27:37 +0530

    [Nagireddy]: added delete last element in the linked list

--- src/test/java/BridgeLabz/LinkedList/MyLinkedListTest.java
+++ src/test/java/BridgeLabz/LinkedList/MyLinkedListTest.java
@@ -7,62 +7,79 @@ import org.junit.jupiter.api.Test;
 
 public class MyLinkedListTest {
 
-    @Test
-    public void given3Nodes_AddThemToTheLinkedList_AndCheckThatTheyAreAddedInCorrectOrder(){
-        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
-        MyNode<Integer> firstNode = new MyNode<>();
-        MyNode<Integer> secondNode = new MyNode<>();
-        MyNode<Integer> thirdNode = new MyNode<>();
-        firstNode.setData(56);
-        secondNode.setData(30);
-        thirdNode.setData(70);
-        myLinkedList.append(firstNode);
-        myLinkedList.append(secondNode);
-        myLinkedList.append(thirdNode);
+        @Test
+        public void given3Nodes_AddThemToTheLinkedList_AndCheckThatTheyAreAddedInCorrectOrder(){
+            MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+            MyNode<Integer> firstNode = new MyNode<>();
+            MyNode<Integer> secondNode = new MyNode<>();
+            MyNode<Integer> thirdNode = new MyNode<>();
+            firstNode.setData(56);
+            secondNode.setData(30);
+            thirdNode.setData(70);
+            myLinkedList.append(firstNode);
+            myLinkedList.append(secondNode);
+            myLinkedList.append(thirdNode);
 
-        boolean testVariable = myLinkedList.getHead().getData().equals(56)
-                && myLinkedList.getHead().getNext().getData().equals(30)
-                && myLinkedList.getTail().getData().equals(70);
-        Assertions.assertTrue(testVariable);
+            boolean testVariable = myLinkedList.getHead().getData().equals(56)
+                    && myLinkedList.getHead().getNext().getData().equals(30)
+                    && myLinkedList.getTail().getData().equals(70);
+            Assertions.assertTrue(testVariable);
+        }
+
+        @Test
+        public void given2Nodes_AddThirdNodeInTheMiddleOfLinkedList_AndCheckThatTheyAreAddedInCorrectOrder(){
+            MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+            MyNode<Integer> firstNode = new MyNode<>();
+            MyNode<Integer> secondNode = new MyNode<>();
+            MyNode<Integer> nodeToInsertInBetweenList = new MyNode<>();
+            firstNode.setData(56);
+            secondNode.setData(70);
+            nodeToInsertInBetweenList.setData(30);
+            myLinkedList.append(firstNode);
+            myLinkedList.append(secondNode);
+            myLinkedList.insert(nodeToInsertInBetweenList , 56);
+
+            boolean testVariable = myLinkedList.getHead().getData().equals(56)
+                    && myLinkedList.getHead().getNext().getData().equals(30)
+                    && myLinkedList.getTail().getData().equals(70);
+            Assertions.assertTrue(testVariable);
+        }
+
+        @Test
+        public void given3Nodes_RemoveHeadNodeOfLinkedList_AndCheckThatListIsInCorrectOrder(){
+            MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+            MyNode<Integer> firstNode = new MyNode<>();
+            MyNode<Integer> secondNode = new MyNode<>();
+            MyNode<Integer> thirdNode = new MyNode<>();
+            firstNode.setData(56);
+            secondNode.setData(30);
+            thirdNode.setData(70);
+            myLinkedList.append(firstNode);
+            myLinkedList.append(secondNode);
+            myLinkedList.append(thirdNode);
+            myLinkedList.popHead();
+
+            boolean testVariable = myLinkedList.getHead().getData().equals(30)
+                    && myLinkedList.getHead().getNext().getData().equals(70);
+            Assertions.assertTrue(testVariable);
+        }
+
+        @Test
+        public void given3Nodes_RemoveTailNodeOfLinkedList_AndCheckThatListIsInCorrectOrder(){
+            MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
+            MyNode<Integer> firstNode = new MyNode<>();
+            MyNode<Integer> secondNode = new MyNode<>();
+            MyNode<Integer> thirdNode = new MyNode<>();
+            firstNode.setData(56);
+            secondNode.setData(30);
+            thirdNode.setData(70);
+            myLinkedList.append(firstNode);
+            myLinkedList.append(secondNode);
+            myLinkedList.append(thirdNode);
+            myLinkedList.popTail();
+
+            boolean testVariable = myLinkedList.getHead().getData().equals(56)
+                    && myLinkedList.getHead().getNext().getData().equals(30);
+            Assertions.assertTrue(testVariable);
+        }
     }
-
-    @Test
-    public void given2Nodes_AddThirdNodeInTheMiddleOfLinkedList_AndCheckThatTheyAreAddedInCorrectOrder(){
-        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
-        MyNode<Integer> firstNode = new MyNode<>();
-        MyNode<Integer> secondNode = new MyNode<>();
-        MyNode<Integer> nodeToInsertInBetweenList = new MyNode<>();
-        firstNode.setData(56);
-        secondNode.setData(70);
-        nodeToInsertInBetweenList.setData(30);
-        myLinkedList.append(firstNode);
-        myLinkedList.append(secondNode);
-        myLinkedList.insert(nodeToInsertInBetweenList , 56);
-
-        boolean testVariable = myLinkedList.getHead().getData().equals(56)
-                && myLinkedList.getHead().getNext().getData().equals(30)
-                && myLinkedList.getTail().getData().equals(70);
-        Assertions.assertTrue(testVariable);
-    }
-
-    @Test
-    public void given3Nodes_RemoveHeadNodeOfLinkedList_AndCheckThatListIsInCorrectOrder(){
-        MyLinkedList<Integer> myLinkedList = new MyLinkedList<>();
-        MyNode<Integer> firstNode = new MyNode<>();
-        MyNode<Integer> secondNode = new MyNode<>();
-        MyNode<Integer> thirdNode = new MyNode<>();
-        firstNode.setData(56);
-        secondNode.setData(30);
-        thirdNode.setData(70);
-        myLinkedList.append(firstNode);
-        myLinkedList.append(secondNode);
-        myLinkedList.append(thirdNode);
-        myLinkedList.popHead();
-
-        boolean testVariable = myLinkedList.getHead().getData().equals(30)
-                && myLinkedList.getHead().getNext().getData().equals(70);
-        Assertions.assertTrue(testVariable);
-    }
-
-
-}
